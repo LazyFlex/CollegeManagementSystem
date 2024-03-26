d131c928581ef7517a45931f8c51c9beda3e739f
Moez Khouaja <magic.and.shark@gmail.com>
2024-03-26 01:58:17 -0400

    Add project files.

---
+++ CollegeManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CollegeManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
